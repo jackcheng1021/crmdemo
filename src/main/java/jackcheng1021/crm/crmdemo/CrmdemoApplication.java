ccc0e04838765a4bf060362e616345d2fd58f07b
jackcheng1021 <jackcheng1021@126.com>
2022-11-09 09:35:45 +0800

    init

---
+++ src/main/java/jackcheng1021/crm/crmdemo/CrmdemoApplication.java
@@ -0,0 +1,14 @@
+package jackcheng1021.crm.crmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmdemoApplication.class, args);
+    }{
+        System.out.println("hello world");
+    }
+}
---
+++ src/test/java/jackcheng1021/crm/crmdemo/CrmdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jackcheng1021.crm.crmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
